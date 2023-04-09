53798e30dc8090cbca7748c918b36dce5a63b66e
Re9Wine <asckortan@gmail.com>
2023-04-09 23:41:44 +0300

    Create test_login_page.py

---
+++ test_login_page.py
@@ -0,0 +1,22 @@
+from .pages.login_page import LoginPage
+
+
+def test_guest_in_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_login_page_contains_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_login_page_contains_registration_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
